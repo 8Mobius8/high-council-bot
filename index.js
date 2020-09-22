9743c82de81190d1294e30bcb0592c6c32fd7392
8Mobius8 <m4rk.odell@gmail.com>
2020-09-21 22:35:20 -0500

    Yeah another git init commit...
    * Setup a basic discord bot that pongs to ping.

---
+++ index.js
@@ -0,0 +1,16 @@
+const config = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Ready!');
+})
+
+client.on('message', message => {
+    if (message.content === config.prefix +"ping") {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('Pong.');
+    }
+})
+
+client.login(config.token)
