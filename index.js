83203600829dde08c295e634a38bfdc20798eaeb
8Mobius8 <m4rk.odell@gmail.com>
2020-09-21 23:13:52 -0500

    Basic stupid commands

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('./config.json');
+const {prefix, token} = require('./config.json');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
@@ -7,10 +7,43 @@ client.once('ready', () => {
 })
 
 client.on('message', message => {
-    if (message.content === config.prefix +"ping") {
-        // send back "Pong." to the channel the message was sent in
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    console.log(`Command: ${command}\nArgs:${args}`)
+
+    if (message.content.startsWith(`${prefix}ping`)) {
         message.channel.send('Pong.');
+    } else if (message.content.startsWith(`${prefix}beep`)) {
+        message.channel.send('Boop.');
+    } else if (message.content.startsWith(`${prefix}server`)) {
+        message.channel.send(`This server's name is: ${message.guild.name}\nTotal Members: ${message.guild.memberCount}`);
+    } else if (message.content.startsWith(`${prefix}user-info`)) {
+        message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+    } else if (command === 'args-info') {
+        if (!args.length) {
+            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+        }
+
+        else if (args[0] === 'foo') {
+            return message.channel.send('bar');
+        }
+
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    } else if (command === 'avatar') {
+        if (!message.mentions.users.size) {
+            return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: "png", dynamic: true })}>`);
+        }
+    
+        const avatarList = message.mentions.users.map(user => {
+            return `${user.username}'s avatar: <${user.displayAvatarURL({ format: "png", dynamic: true })}>`;
+        });
+    
+        // send the entire array of strings as a message
+        // by default, discord.js will `.join()` the array with `\n`
+        message.channel.send(avatarList);
     }
 })
 
-client.login(config.token)
+client.login(token)
