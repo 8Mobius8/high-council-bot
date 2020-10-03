8331748b2816db33546302252b776f1704ebc665
8Mobius8 <m4rk.odell@gmail.com>
2020-10-02 20:23:48 -0500

    feat(avatar): avatar command guildonly property
    * Add new avatar that prints the command users' avatar and roles
    * Add `guildOnly` property to stop commands from running in direct
      messages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 const Discord = require('discord.js')
-const { prefix, token } = require('./config.js')
+const { prefix, token, listenServer } = require('./config.js')
 const { salutations } = require('./commands/phrases.json')
 
 const client = new Discord.Client()
@@ -28,9 +28,9 @@ client.on('message', message => {
   const commandName = args.shift().toLowerCase()
 
   const command = client.commands.get(commandName) ||
-		client.commands.find(cmd => {
-		  return cmd.aliases && cmd.aliases.includes(commandName)
-		})
+    client.commands.find(cmd => {
+      return cmd.aliases && cmd.aliases.includes(commandName)
+    })
 
   if (!command) return
 
@@ -63,6 +63,10 @@ client.on('message', message => {
   timestamps.set(message.author.id, now)
   setTimeout(() => timestamps.delete(message.author.id), cooldownAmount)
 
+  if (command.guildOnly && message.channel.type === 'dm') {
+    return message.reply('I can\'t execute that command inside DMs!')
+  }
+
   try {
     command.execute(message, args)
   } catch (error) {
@@ -75,7 +79,7 @@ client.login(token)
   .catch((error) => {
     if (error.code === 'TOKEN_INVALID') {
       console.error('Discord token is not acceptable. Please set config.json or DISCORD_TOKEN appropiately.')
-    } else	{
+    } else {
       throw error
     }
   })
@@ -84,10 +88,12 @@ function randomFrom (anArray) {
   return anArray[Math.floor(Math.random() * anArray.length)]
 }
 
-http.createServer(
-  function (request, response) {
-    const content = randomFrom(salutations)
-    response.writeHead(200)
-    response.end(content, 'utf-8')
-  }
-).listen(process.env.PORT || 443)
+if (listenServer) {
+  http.createServer(
+    function (request, response) {
+      const content = randomFrom(salutations)
+      response.writeHead(200)
+      response.end(content, 'utf-8')
+    }
+  ).listen(process.env.PORT || 443)
+}
