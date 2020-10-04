2fc91a2cceed855aa8cc9e1a1f9b308931827a63
Mark Odell <m4rk.odell@gmail.com>
2020-10-04 18:31:36 -0500

    feat(version): auto update versiondb for version cmd
    Tries to use semantic release to update version db json to be read by version command

--- change-db.js
+++ change-db.js
@@ -1,1 +1,19 @@
 console.log('run change-db.js')
+
+const args = process.argv.slice(2).join('')
+
+try {
+  const versiondb = JSON.parse(args)
+
+  const data = JSON.stringify(versiondb);
+
+  fs.writeFile('./commands/versions.json', data, (err) => {
+      if (err) {
+          throw err;
+      }
+      console.log("JSON data is saved.");
+  })
+
+} catch(err) {
+  console.error(err)
+}
--- commands/version.js
+++ commands/version.js
@@ -1,10 +1,22 @@
 const { versionName, version } = require('../package.json')
+const { releases } = require('./versions.json')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   name: 'version',
   description: 'Prints version info for the bot.',
   cooldown: 5,
   execute (message, args) {
-    message.channel.send(`${versionName} - ${version}`)
+    const theRelease = releases.find(release => release.version === version)
+
+    const avatarEmbed = new MessageEmbed()
+      .setTitle(`${versionName} - ${version}`)
+      .setThumbnail(message.client.user.displayAvatarURL({ dynamic: true, size: 128 }))
+      .setURL(theRelease.url)
+      .addField(
+        'Release Notes',
+        theRelease.notes
+      )
+    message.channel.send(avatarEmbed)
   }
 }
