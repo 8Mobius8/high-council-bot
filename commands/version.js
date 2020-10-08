9eb65b94e812dc13e0825d1c492e1bb03021993a
Mark Odell <m4rk.odell@gmail.com>
2020-10-08 16:32:07 -0500

    feat(version): version all print all release notes
    Parses full the release notes from the change log to do this.

--- commands/version.js
+++ commands/version.js
@@ -1,5 +1,5 @@
 const { version } = require('../package.json')
-const { releases } = require('./versions.json')
+const releases = require('./versions.json')
 const { MessageEmbed } = require('discord.js')
 
 module.exports = {
@@ -7,16 +7,38 @@ module.exports = {
   description: 'Prints version info for the bot.',
   cooldown: 5,
   execute (message, args) {
-    const theRelease = releases.find(release => release.version === version)
-    const theSections = theRelease.sections.map((i, s) => {
-      return { name: s.name, value: s.notes }
-    })
-
     const avatarEmbed = new MessageEmbed()
-      .setTitle(`${version}`)
       .setThumbnail(message.client.user.displayAvatarURL({ dynamic: true, size: 128 }))
-      .setURL(theRelease.url)
-      .addFields(theSections)
+
+    const toshow = []
+
+    if (args.indexOf('all') > -1) {
+      avatarEmbed.setTitle('Release Notes')
+      toshow.push(...releases)
+
+      avatarEmbed.description = ''
+      for (const release of toshow) {
+        avatarEmbed.description += `[${release.version}](${release.url}) | `
+        let notes = ''
+        for (const section of release.sections) {
+          notes += `**${section.name}**\n${section.notes}\n\n`
+        }
+        avatarEmbed.addField(`${release.version}`, notes)
+      }
+      avatarEmbed.description = avatarEmbed.description.substr(0, avatarEmbed.description.length - 3)
+    } else {
+      toshow.push(releases.find(release => release.version === version))
+      avatarEmbed
+        .setTitle(`${version}`)
+        .setURL(toshow[0].url)
+
+      for (const release of toshow) {
+        for (const section of release.sections) {
+          avatarEmbed.addField(section.name, section.notes)
+        }
+      }
+    }
+
     message.channel.send(avatarEmbed)
   }
 }
