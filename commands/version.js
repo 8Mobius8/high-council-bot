c4cb6d40704bd1282b1305a45c64a7e74afea8da
Mark Odell <m4rk.odell@gmail.com>
2020-10-09 14:43:01 -0500

    refactor(version): refactor version command

--- commands/version.js
+++ commands/version.js
@@ -1,44 +1,58 @@
 const { version } = require('../package.json')
-const releases = require('./versions.json')
+const botReleases = require('./versions.json')
 const { MessageEmbed } = require('discord.js')
+
+const options = ['all']
 
 module.exports = {
   name: 'version',
   description: 'Prints version info for the bot.',
   cooldown: 5,
   execute (message, args) {
-    const avatarEmbed = new MessageEmbed()
+    const versionMessage = new MessageEmbed()
       .setThumbnail(message.client.user.displayAvatarURL({ dynamic: true, size: 128 }))
 
-    const toshow = []
-
-    if (args.indexOf('all') > -1) {
-      avatarEmbed.setTitle('Release Notes')
-      toshow.push(...releases)
-
-      avatarEmbed.description = ''
-      for (const release of toshow) {
-        avatarEmbed.description += `[${release.version}](${release.url}) | `
-        let notes = ''
-        for (const section of release.sections) {
-          notes += `**${section.name}**\n${section.notes}\n\n`
-        }
-        avatarEmbed.addField(`${release.version}`, notes)
-      }
-      avatarEmbed.description = avatarEmbed.description.substr(0, avatarEmbed.description.length - 3)
-    } else {
-      toshow.push(releases.find(release => release.version === version))
-      avatarEmbed
-        .setTitle(`${version}`)
-        .setURL(toshow[0].url)
-
-      for (const release of toshow) {
-        for (const section of release.sections) {
-          avatarEmbed.addField(section.name, section.notes)
-        }
-      }
+    const unsupportedArgs = args.filter(a => options.indexOf(a) === -1)
+    if (unsupportedArgs.length > 0) {
+      message.channel.send(`Invalid version options were given:\n${unsupportedArgs}\n\nTry using these: \`${options}\``)
+      return
     }
 
-    message.channel.send(avatarEmbed)
+    const inputOptions = args.filter(a => options.indexOf(a) > -1)
+    buildVersions(versionMessage, filterReleases(inputOptions, botReleases))
+
+    message.channel.send(versionMessage)
+  }
+}
+
+function filterReleases (options, releases) {
+  if (options.indexOf('all') > -1) return releases
+  else return releases.slice(0, 1)
+}
+
+function buildVersions (embeded, releases) {
+  if (releases.length === 1) {
+    buildVersion(embeded, releases[0])
+    return
+  }
+
+  embeded.title = 'Release Notes'
+  embeded.description = ''
+  for (const release of releases) {
+    embeded.description += `[${release.version}](${release.url}) | `
+    let notes = ''
+    for (const section of release.sections) {
+      notes += `**${section.name}**\n${section.notes}\n\n`
+    }
+    embeded.fields.push({ name: `${release.version}`, value: notes })
+  }
+  embeded.description = embeded.description.slice(0, -3)
+}
+
+function buildVersion (embeded, release) {
+  embeded.title = `${version}`
+  embeded.url = release.url
+  for (const section of release.sections) {
+    embeded.fields.push({ name: section.name, value: section.notes })
   }
 }
